4f2bc66ede59b18424cc9f508815cff1510f9850
Mo-fopefoluwa <fopefaokunla@gmail.com>
2023-06-02 15:20:34 -0700

    fops

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import './App.css';
-import {Routes } from "react-router-dom";
-import { Route} from "react-router-dom";
+//import {Routes } from "react-router-dom";
+//import { Route} from "react-router-dom";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Services from "./components/Services/Services";
 import Skills from "./components/Skills/Skills";
@@ -13,37 +13,16 @@ import Skills from "./components/Skills/Skills";
 function App() {
   return (
     <div className='App'>
-      <Routes >
-   
-          <Route path='/' element={<Header />}>
-      
-      </Route>
-     
-          <Route path='/Hero'  element={<Hero />}>
-          
-      </Route>
-      
-          <Route path='/Services'  element={<Services />}>
-         
-      </Route>
-      
-          <Route path='/Experience'  element={<Experience />}>
-          
-      </Route>
-      
-          <Route path='/Portfolio'  element={<Portfolio />}>
-
-      </Route>
-      
-          <Route path='/Skills' element={<Skills />}>
-         
-      </Route>
-      
-          <Route path='/Footer'  element={<Footer />}>
-        
-      </Route>
-  </Routes >
+  
+  <Header />
+  <Hero />
+  <Services />
+  <Experience />
+  <Portfolio />
+  <Skills />
+  <Footer />
       </div>
+
   );
 }
 
